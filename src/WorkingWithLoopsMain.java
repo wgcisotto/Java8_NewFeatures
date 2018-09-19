7b85b41360a6d0d802a62542545bf9a0291e25aa
William Galindo Cisotto <wgcisotto@orizon.com.br>
2018-09-19 14:30:31 -0300

    Testes do capitulo 2 e 3

--- src/WorkingWithLoopsMain.java
+++ src/WorkingWithLoopsMain.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
-public class Chapter2 {
+public class WorkingWithLoopsMain {
 
     public static void main (String ... args){
 
